484b26ef86a6adda61addfcd08b4f09248b516ef
soorajUM <soorajushamurali13@gmail.com>
2024-02-13 22:16:01 -0800

    todolist converted to component

--- src/Routes/New/index.tsx
+++ src/Routes/New/index.tsx
@@ -1,79 +1,12 @@
-import Button from "../../Components/Button";
-import { useRef, useState } from "react";
+import Card from "../../Components/Card";
+import Dropdown from "../../Components/Dropdown";
+import Label from "../../Components/Input/Components/Label";
+import TodoList from "../../Components/TodoList";
 
 const New = () => {
-  const [todo, setTodo] = useState<string>("");
-  const [todos, setTodos] = useState<any>([]);
-  const [editindex, seteditindex] = useState<number | null>(null);
-
   return (
-    <div className="main flex-col place-items-center flex bg-violet-200 h-screen">
-      <div className="input">
-        <input
-          className=" w-[500px] m-4 p-4 rounded-2xl shadow-md"
-          type="text"
-          value={todo}
-          onChange={(e) => {
-            setTodo(e.target.value);
-          }}
-        />
-        <button
-          className="bg-violet-400 p-2 rounded-lg shadow-md"
-          onClick={() => {
-            if (editindex !== null) {
-              todos[editindex] = todo;
-            } else {
-              setTodos([...todos, todo]);
-            }
-            seteditindex(null);
-            setTodo("");
-          }}
-        >
-          add
-        </button>
-      </div>
-      <div>
-        <ul className="">
-          <div className="w-[600px] overflow-hidden m-4 p-4">
-            {todos.map((element: string, index: number) => (
-              <div
-                id="todo"
-                className=" flex gap-5 justify-between px-6 py-7 bg-white rounded-2xl shadow-md max-w m-4 "
-              >
-                <div id="content" className="overflow-hidden flex items-center">
-                  <li className="" key={index}>
-                    {element}
-                  </li>
-                </div>
-                <div id="option" className="bg-white flex px-4">
-                  <img
-                    loading="lazy"
-                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/50569d44fa30da420b553304ce9374ff5f46606f9e42b1da28eb244cca779a03?"
-                    className="aspect-square w-[25px] cursor-pointer"
-                    onClick={() => {
-                      setTodo(element);
-                      seteditindex(index);
-                    }}
-                  />
-                  <img
-                    loading="lazy"
-                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/471f8cb1ba3459175174c01db3fe20b9216d64b645f163831e28acce2c0ddbf1?"
-                    className="aspect-square w-[25px] cursor-pointer"
-                    onClick={() =>
-                      setTodos(
-                        todos.filter((element: string, i: number) => {
-                          console.log(index, i);
-                          return i !== index;
-                        })
-                      )
-                    }
-                  />
-                </div>
-              </div>
-            ))}
-          </div>
-        </ul>
-      </div>
+    <div>
+      <TodoList />
     </div>
   );
 };
--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,17 +1,14 @@
-import {Route,Routes} from "react-router-dom"
-import Home from "./Home"
-import New from "./New"
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import New from "./New";
 
 const Router = () => {
   return (
     <Routes>
-        <Route path="" element={<Home/>} />
-        <Route path="/hello" element={<div>Hello</div>} />
-        <Route path="/New" element={<New/>} />
-        
+      <Route path="" element={<Home />} />
+      <Route path="/New" element={<New />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default Router
+export default Router;
